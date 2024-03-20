9cd3ce2248cf70e26af265a705059e0040e016c9
Shivam <kalrashivam47@gmail.com>
2024-03-21 00:37:00 +0530

    public

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
   return (
     <div>
       <div className="hero relative w-screen">
-        <img src="src\assets\frontend-images\hero2.jpg" className="w-screen" />
+        <img src="public\frontend-images\hero2.jpg" className="w-screen" />
         <div className="overlay bg-black/35 w-screen absolute left-0 top-0 right-0 bottom-0"></div>
         {/* <div className="explore absolute right-36 bottom-1/3 border-4 border-white text-white font-medium px-20 py-7 text-3xl"><Link>Explore</Link></div> */}
       </div>
@@ -41,10 +41,7 @@ const Home = () => {
         <div className="card relative overflow-hidden flex-column justify-between align-middle w-[500px] mx-10">
           <Link>
             <div className="image">
-              <img
-                src="src\assets\frontend-images\1.jpg"
-                className=" w-[500px]"
-              />
+              <img src="public\frontend-images\1.jpg" className=" w-[500px]" />
             </div>
             <div className="title text-2xl font-thin text-center left-10 right-10 bottom-10 p-3 absolute bg-white">
               <Link>Title</Link>
@@ -54,10 +51,7 @@ const Home = () => {
 
         <div className="card relative overflow-hidden flex-column justify-between align-middle w-[500px] mx-10">
           <div className="image">
-            <img
-              src="src\assets\frontend-images\2.jpg"
-              className=" w-[500px]"
-            />
+            <img src="public\frontend-images\2.jpg" className=" w-[500px]" />
           </div>
           <div className="title text-2xl font-thin text-center left-10 right-10 bottom-10 p-3 absolute bg-white">
             <Link>Title</Link>
@@ -66,10 +60,7 @@ const Home = () => {
 
         <div className="card relative overflow-hidden  flex-column justify-between align-middle w-[500px] mx-10 ">
           <div className="image">
-            <img
-              src="src\assets\frontend-images\3.jpg"
-              className=" w-[500px]"
-            />
+            <img src="public\frontend-images\3.jpg" className=" w-[500px]" />
           </div>
           <div className="title text-2xl font-thin text-center left-10 right-10 bottom-10 p-3 absolute bg-white">
             <Link>Title</Link>
@@ -79,10 +70,7 @@ const Home = () => {
 
       <div className="intro relative overflow-hidden flex justify-center align-middle w-screen my-20 mx-10 ">
         <div className="image">
-          <img
-            src="src\assets\frontend-images\intro.jpg"
-            className=" w-[700px]"
-          />
+          <img src="public\frontend-images\intro.jpg" className=" w-[700px]" />
         </div>
 
         <div className="flex-column justify-between align-middle w-[40%] ml-10 ">
